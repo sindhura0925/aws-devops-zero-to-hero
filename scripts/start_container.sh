402e849306c69549fb117bba094f64260d801031
sindhura0925 <143525665+sindhura0925@users.noreply.github.com>
2025-09-20 13:30:17 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull sindhura0925/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sindhura0925/simple-python-flask-app
+docker run -d -p 5100:5000 sindhura0925/simple-python-flask-app
